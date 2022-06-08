4afd88de537177db90138f52d461d5888881c7a7
FranciscoGreenaway <f.greenaway@outlook.com>
2022-06-07 20:52:11 -0400

    Handling (cannot import name 'db' from partially initialized module 'app') Error

---
+++ app.py
@@ -0,0 +1,38 @@
+from api import db, app
+from ariadne import ObjectType, load_schema_from_path, make_executable_schema, snake_case_fallback_resolvers, \
+    graphql_sync
+from ariadne.constants import PLAYGROUND_HTML
+from flask import request, jsonify
+
+from api.queries import get_employees_resolver, get_employee_by_id_resolver
+
+
+query = ObjectType("Query")
+query.set_field("getEmployees", get_employees_resolver)
+query.set_field("getEmployeeByID", get_employee_by_id_resolver)
+
+# Load GraphQL schema
+type_defs = load_schema_from_path("schema.graphql")
+
+# Make executable schema by binding python snake case to schema and query. ex. "True id" -> "true_id"
+schema = make_executable_schema(
+    type_defs, query, snake_case_fallback_resolvers
+)
+
+
+@app.route("/graphql", methods=['GET'])     # Loads up GraphQL UI for us
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
+
+
+@app.route("/graphql", methods=['POST'])    # Handling POST Request
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
